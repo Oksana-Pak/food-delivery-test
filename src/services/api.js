bb954b557cc2cda7eee35e72bc26ce04cbc4161a
Oksana-Pak <okspak7@gmail.com>
2023-06-08 10:26:48 +0300

    add new https

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 import { errorMessage } from "./notifications";
-// axios.defaults.baseURL = 'https://64651a5b228bd07b35426720.mockapi.io';
-axios.defaults.baseURL = "http://localhost:3000";
+
+axios.defaults.baseURL = "https://food-delivery-test.onrender.com";
 
 export const fetchDishes = async (page, shop) => {
   try {
     const response = await axios.get(`/${shop}?page=${page}`);
     return response.data;
-
-    // console.log(response.data);
   } catch (error) {
     errorMessage("Something wrong. Try again.");
     throw new Error("Something wrong. Try again.");
@@ -19,8 +17,6 @@ export const addOrder = async (order) => {
   try {
     const response = await axios.post(`/user`, order);
     return response.data;
-
-    // console.log(response.data);
   } catch (error) {
     errorMessage("Something wrong. Try again.");
     throw new Error("Something wrong. Try again.");
